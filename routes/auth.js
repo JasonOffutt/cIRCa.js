362033910d0034cfa02458d7ddd0aff383815bd3
JasonOffutt <jason.offutt@gmail.com>
2012-08-02 16:51:57 -0700

    Removing duplicated code, refining routing/auth patterns a little bit.

--- routes/auth.js
+++ routes/auth.js
@@ -10,90 +10,71 @@ var authService = require('../lib/authService');
 // Google OAuth 2.0 docs: https://developers.google.com/accounts/docs/OAuth2WebServer
 // Facebook OAuth 2.0 docs: https://developers.facebook.com/docs/authentication/server-side/
 
-// GET /auth/twitter
-exports.twitterAuth = function (req, res) {
-	authService.login('twitter', function (err, oauth_token, oauth_token_secret, results) {
-		if (err) {
-			console.log(err);
-		} else {
-			req.session.oauth = {
-				token: oauth_token,
-				token_secret: oauth_token_secret
-			};
-			res.redirect('https://twitter.com/oauth/authenticate?oauth_token=' + oauth_token);
-		}
-	});
-};
-
-exports.twitterCallback = function (req, res) {
-	if (!req.session.oauth) {
-		return next(new Error('Are you lost?'));
-	}
-
-	req.session.oauth.verifier = req.query.oauth_verifier;
-	var oauth = req.session.oauth;
-	authService.authToken(
-		'twitter',
-		{
-			token: oauth.token,
-			token_secret: oauth.token_secret,
-			verifier: oauth.verifier
-		},
-		function (err, oauth_access_token, oauth_access_token_secret, results) {
+var providerUrls = {
+		twitter: 'https://twitter.com/oauth/authenticate?oauth_token=',
+		facebook: '',
+		google: 'https://www.googleapis.com/oauth2/v1/userinfo?access_token='
+	},
+	handleAuthRequest = function (provider, req, res) {
+		authService.login(provider, function (err, oauth_token, oauth_token_secret, results) {
 			if (err) {
 				console.log(err);
 			} else {
-				req.session.oauth.access_token = oauth_access_token;
-				res.redirect('/bots');
+				req.session.oauth = {
+					token: oauth_token,
+					token_secret: oauth_token_secret
+				};
+				res.redirect(providerUrls[provider] + oauth_token);
 			}
-	});
+		});
+	},
+	handleCallback = function (provider, req, res) {
+		if (!req.session.oauth) {
+			return next(new Error('Are you lost?'));
+		}
+
+		req.session.oauth.verifier = req.query.oauth_verifier;
+		var oauth = req.session.oauth;
+		authService.authToken(
+			provider,
+			{
+				token: oauth.token,
+				token_secret: oauth.token_secret,
+				verifier: oauth.verifier
+			},
+			function (err, oauth_access_token, oauth_access_token_secret, results) {
+				if (err) {
+					console.log(err);
+				} else {
+					req.session.oauth.access_token = oauth_access_token;
+					res.redirect('/bots');
+				}
+			});
+	};
+
+// GET /auth/twitter
+exports.twitterAuth = function (req, res) {
+	handleAuthRequest('twitter', req, res);
+};
+
+exports.twitterCallback = function (req, res) {
+	handleCallback('twitter', req, res);
 };
 
 // GET /auth/facebook
 exports.facebookAuth = function (req, res) {
-	// TODO: Handle this route with a redirect?
+	handleAuthRequest('facebook', req, res);
 };
 
 exports.facebookCallback = function (req, res) {
-
+	handleCallback('facebook', req, res);
 };
 
 // GET /auth/google
 exports.googleAuth = function (req, res) {
-	authService.login('google', function (err, oauth_token, oauth_token_secret, results) {
-		if (err) {
-			console.log(err);
-		} else {
-			req.session.oauth = {
-				token: oauth_token,
-				token_secret: oauth_token_secret
-			};
-			res.redirect('https://www.googleapis.com/oauth2/v1/userinfo?access_token=' + oauth_token);
-		}
-	});
+	handleAuthRequest('google', req, res);
 };
 
 exports.googleCallback = function (req, res) {
-	if (!req.session.oauth) {
-		return next(new Error('Are you lost?'));
-	}
-
-	req.session.oauth.verifier = req.query.oauth_verifier;
-	var oauth = req.session.oauth;
-	oauthService.authToken(
-		'google',
-		{
-			token: oauth.token,
-			token_secret: oauth.token_secret,
-			verifier: oauth.verifier
-		}, 
-		function (err, oauth_access_token, oauth_access_token_secret, results) {
-			if (err) {
-				console.log(err);
-			} else {
-				req.session.oauth.access_token = oauth_access_token;
-				res.redirect('/bots');
-			}
-		}
-	);
+	handleCallback('google', req, res);
 };
