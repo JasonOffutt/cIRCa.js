ebc77408f9412c02e340072d538cfdaa616f5f24
JasonOffutt <jason.offutt@gmail.com>
2012-08-09 16:15:00 -0700

    Creating more views, fleshing out bot creation workflow.

--- routes/bots.js
+++ routes/bots.js
@@ -1,5 +1,13 @@
 'use strict';
 
+var botRepository = require('../lib/persistenceService').bots;
+
 exports.index = function (req, res) {
-	res.render('botlist', { title: 'Bot List' });
+	botRepository.findByUserId(req.session.userId, function (err, bots) {
+		res.render('botlist', { title: 'Bot List', bots: bots });
+	});
+};
+
+exports.new = function (req, res) {
+	res.render('newbot', { title: 'New Bot' })
 };
