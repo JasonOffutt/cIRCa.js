125a7c1dc34ec9f7e14014d3eb5c736c73444ce0
JasonOffutt <jason.offutt@gmail.com>
2012-08-09 21:55:10 -0700

    Moving views into folder structure

--- routes/bots.js
+++ routes/bots.js
@@ -4,10 +4,10 @@ var botRepository = require('../lib/persistenceService').bots;
 
 exports.index = function (req, res) {
 	botRepository.findByUserId(req.session.userId, function (err, bots) {
-		res.render('botlist', { title: 'Bot List', bots: bots });
+		res.render('bots/botlist', { title: 'Bot List', bots: bots });
 	});
 };
 
 exports.new = function (req, res) {
-	res.render('newbot', { title: 'New Bot' })
+	res.render('bots/newbot', { title: 'New Bot' })
 };
--- routes/users.js
+++ routes/users.js
@@ -1,5 +1,5 @@
 'use strict';
 
 exports.index = function (req, res) {
-	res.render('userprofile', { title: 'User Profile' });
+	res.render('users/userprofile', { title: 'User Profile' });
 };
